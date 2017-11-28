729d2648c888f4a4a19bd5a992a1c9db107a6d7a
Iva94 <ivana.jovovic94@gmail.com>
2017-11-28 23:13:36 +0100

    Restaurant Map v1.0

--- ember/app/components/google-map.js
+++ ember/app/components/google-map.js
@@ -14,6 +14,7 @@ export default Ember.Component.extend({
     let options = {
       center: new window.google.maps.LatLng(43.9, 18),
       zoom: 7,
+	  mapTypeId: 'roadmap',
     };
 
     let map = new window.google.maps.Map(container, options);
